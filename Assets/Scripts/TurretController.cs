ddb3ca9edea0d05a55dfc3524cdfe3a9e7ab9268
edster92 <edster.r92@gmail.com>
2018-04-15 17:27:38 +0300

    Added turrent controller and base weapon scripts

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : StateManager {
+
+
+
+	public enum TurretStages // доступные состояния турели
+	{
+		Idle,
+		Active
+	}
+
+	public void SearchTarget () // здесь ищем обьекты в зоне действия турели
+	{
+		
+	}
+
+	public void TurretFire () // стреляем в найденные обьекты
+	{
+		
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) // реагируем на тригерры в зоне коллайдера
+	{
+		
+		SearchTarget ();
+	}
+
+}
+
+[System.Serializable]
+public class TargetData
+{
+	public float AgressionKoef = 0f;
+	public BaseGameObject Object;
+}
---
+++ Assets/Scripts/WeaponScripts/BaseWeapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeapon : MonoBehaviour {
+	
+	protected bool fireOnOff = false;
+
+	[SerializeField]
+	protected float [] SpeedFire;
+	protected float lastFireTime;
+	[SerializeField]
+	protected float[] damage;
+	protected Rigidbody2D player;
+
+
+	public virtual void FireStart ()
+	{
+		fireOnOff = true;
+		StartCoroutine (FireUpdate());
+	}
+
+	protected virtual IEnumerator FireUpdate ()
+	{
+		yield return null;
+	}
+
+	public virtual void FireEnd ()
+	{
+		StopCoroutine (FireUpdate());
+		fireOnOff = false;
+	}
+
+}
+
+
